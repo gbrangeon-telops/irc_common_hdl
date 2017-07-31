ece894d3bc31ed1ac32b8020680aaedfdbb3ecd7
enofodjie-telpos <edem.nofodjie@telops.com>
2017-07-31 16:39:47 +0000

    git-svn-id: http://einstein/svn/firmware/Common_HDL/trunk@20732 3e98269d-ff51-e449-abca-491a6567d830

--- Common_Projects/TEL2000/FPA_common/src/fpa_status_gen.vhd
+++ Common_Projects/TEL2000/FPA_common/src/fpa_status_gen.vhd
@@ -110,6 +110,7 @@ architecture rtl of fpa_status_gen is
    signal fpa_prog_init_done             : std_logic;
    signal fpa_driver_cmd_in_err          : std_logic_vector(7 downto 0);
    signal flegx_present                  : std_logic;
+   signal fpa_readout_err                : std_logic_vector(1 downto 0);
    
    
    component sync_reset
@@ -194,7 +195,8 @@ begin
    -----------------------------------------------
    -- FPA_DRIVER_STAT                             
    -----------------------------------------------
-   
+   fpa_readout_err(1)    <= FPA_DRIVER_STAT(19);
+   fpa_readout_err(0)    <= FPA_DRIVER_STAT(18);
    fpa_prog_init_done    <= FPA_DRIVER_STAT(17);            -- monte à '1' lorsque la config d'initialisation est programmée dans le ROIC. Ce qui est intéressant pour les ROIC necessitant une config d'initialisation 
    fpa_driver_dvalid_err <= FPA_DRIVER_STAT(16);            -- upour les sofradir: un fpa_data_valid est manquant après integration
    fpa_driver_cmd_in_err <= FPA_DRIVER_STAT(15 downto 8);   -- pour les détecteurs numériques principalement, c'est l'ID de la cmd en erreur
@@ -290,7 +292,9 @@ begin
             fpa_init_success <= and_reduce(fpa_serdes_success);
             
             -- les erreurs à latcher (connecter les signaux des erreurs ici)
-            error(31 downto 16) <= (others => '0');  -- non utilisés
+            error(31 downto 18) <= (others => '0');  -- non utilisés 
+            error(17) <= fpa_readout_err(1);
+            error(16) <= fpa_readout_err(0);
             error(15) <= fpa_driver_dvalid_err;
             error(14) <= fpa_driver_trig_err;
             error(13) <= fpa_driver_ram_err;
@@ -468,11 +472,11 @@ begin
                
                when  x"005C" =>   -- flex_flegx_present
                   stat_read_reg <= resize('0'& flex_flegx_present, 32);
-               -----------------------------------
+                  -----------------------------------
                
                when  x"0060" =>   -- cmd en erreur
                   stat_read_reg <= resize(fpa_driver_cmd_in_err, 32);
-               
+                  
                -- FPA_SERDES_STAT
                when  x"0064" =>   -- fpa serdes done
                   stat_read_reg <=  resize(fpa_serdes_done, 32);
@@ -482,9 +486,9 @@ begin
                
                when  x"006C" =>   -- fpa serdes delay
                   stat_read_reg <=  resize(fpa_serdes_delay(3), 8) & 
-                                    resize(fpa_serdes_delay(2), 8) & 
-                                    resize(fpa_serdes_delay(1), 8) & 
-                                    resize(fpa_serdes_delay(0), 8);
+                  resize(fpa_serdes_delay(2), 8) & 
+                  resize(fpa_serdes_delay(1), 8) & 
+                  resize(fpa_serdes_delay(0), 8);
                
                when  x"0070" =>   -- fpa serdes edges ch0
                   stat_read_reg <=  resize(fpa_serdes_edges(0), 32);
@@ -497,7 +501,7 @@ begin
                
                when  x"007C" =>   -- fpa serdes edges ch3
                   stat_read_reg <=  resize(fpa_serdes_edges(3), 32);
-               
+                  
                -- FPA init status
                when  x"0080" =>   -- fpa init done
                   stat_read_reg <=  (0 => fpa_init_done, others => '0');
@@ -507,6 +511,67 @@ begin
                
                when  x"0088" =>   -- prog_init_done
                   stat_read_reg <=  (0 => fpa_prog_init_done, others => '0');
+                  
+--               -- verification
+--               when x"0100" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_diag_mode              , 32));
+--               when x"0104" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_diag_type              , 32));
+--               when x"0108" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_pwr_on                 , 32));
+--               when x"010C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_trig_ctrl_mode         , 32));
+--               when x"0110" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_acq_trig_ctrl_dly      , 32));
+--               when x"0114" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_acq_trig_period_min    , 32));
+--               when x"0118" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_xtra_trig_ctrl_dly     , 32));
+--               when x"011C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_xtra_trig_period_min   , 32));
+--               when x"0120" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.comn.fpa_stretch_acq_trig       , 32));             
+--               when x"0124" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.diag.ysize                      , 32));
+--               when x"0128" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.diag.xsize_div_tapnum           , 32));                
+--               when x"012C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.roic.ystart                     , 32));
+--               when x"0130" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.roic.ysize_div4_m1              , 32));                 
+--               when x"0134" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdet_code                       , 32));
+--               when x"0138" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.ref_mode_en                     , 32));
+--               when x"013C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.ref_chn_en                      , 32));
+--               when x"0140" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.itr                             , 32));
+--               when x"0144" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.real_mode_active_pixel_dly      , 32));              
+--               when x"0148" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.speedup_lsync                   , 32));
+--               when x"014C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.speedup_sample_row              , 32));
+--               when x"0150" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.speedup_unused_area             , 32));        
+--               when x"0154" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.line_start_num         , 32));
+--               when x"0158" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.line_end_num           , 32));
+--               when x"015C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.sof_posf_pclk          , 32));
+--               when x"0160" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.eof_posf_pclk          , 32));
+--               when x"0164" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.sol_posl_pclk          , 32));
+--               when x"0168" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.eol_posl_pclk          , 32));
+--               when x"016C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.eol_posl_pclk_p1       , 32));
+--               when x"0170" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.window_lsync_num       , 32));
+--               when x"0174" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.line_period_pclk       , 32));
+--               when x"0178" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.raw_area.readout_pclk_cnt_max   , 32));   
+--               when x"017C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.user_area.line_start_num        , 32));
+--               when x"0180" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.user_area.line_end_num          , 32));
+--               when x"0184" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.user_area.sol_posl_pclk         , 32));
+--               when x"0188" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.user_area.eol_posl_pclk         , 32));
+--               when x"018C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.user_area.eol_posl_pclk_p1      , 32));      
+--               when x"0190" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.pix_samp_num_per_ch             , 32));
+--               when x"0194" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.hgood_samp_sum_num              , 32));
+--               when x"0198" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.hgood_samp_mean_numerator       , 32));
+--               when x"019C" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vgood_samp_sum_num              , 32));
+--               when x"01A0" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vgood_samp_mean_numerator       , 32));
+--               when x"01A4" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.good_samp_first_pos_per_ch      , 32));
+--               when x"01A8" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.good_samp_last_pos_per_ch       , 32));            
+--               when x"01AC" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(1)                   , 32));
+--               when x"01B0" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(2)                   , 32));
+--               when x"01B4" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(3)                   , 32));
+--               when x"01B8" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(4)                   , 32));
+--               when x"01BC" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(5)                   , 32));
+--               when x"01C0" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(6)                   , 32));
+--               when x"01C4" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(7)                   , 32));
+--               when x"01C8" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.vdac_value(8)                   , 32));
+--               when x"01CC" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.adc_clk_phase(1)                , 32));
+--               when x"01D0" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.adc_clk_phase(2)                , 32));
+--               when x"01D4" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.adc_clk_phase(3)                , 32));
+--               when x"01D8" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.adc_clk_phase(4)                , 32));             
+--               when x"01DC" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.lsydel_mclk                     , 32));
+--               when x"01E0" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.boost_mode                      , 32));
+--               when x"01E4" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.speedup_lsydel                  , 32));
+--               when x"01E8" => stat_read_reg <= std_logic_vector(resize(FPA_INTF_CFG.adc_clk_pipe_sync_pos          , 32));
                
                when others =>   
                   stat_read_reg <= (others => '0');
@@ -524,3 +589,17 @@ begin
    
    
 end rtl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
