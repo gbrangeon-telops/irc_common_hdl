0b0191aa348585bcc36f531e3ce7fa47a56a3466
enofodjie-telpos <edem.nofodjie@telops.com>
2019-08-26 16:01:55 +0000

    fastrd2: chantier
    git-svn-id: http://einstein/svn/firmware/Common_HDL/trunk@24064 3e98269d-ff51-e449-abca-491a6567d830

--- Common_Projects/TEL2000/FPA_common/src/fastrd2/compil_fastrd2_common.do
+++ Common_Projects/TEL2000/FPA_common/src/fastrd2/compil_fastrd2_common.do
@@ -6,15 +6,22 @@
  
 # sources fastrd2 common 
 acom -nowarn DAGGEN_0523 -incr \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_define.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_clk_mem_ctrler.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_raw_area_gen.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_stretching_area_gen.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_user_area_gen.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_waste_area_gen.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_wdow_info_rate_core.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_wdow_mem_input_ctler.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_wdow_mem_output_ctler.vhd \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_clks_memory.bde \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_wdow_info_rate_ctrler.bde \
- d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2_wdow_mem.bde 
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_define.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_raw_area_gen.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_user_area_gen.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_clk_area_gen.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_area_mem_ctler.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_area_flow_gen.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_misc_flags_gen.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_area_mem.bde
+ 
+ 
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_wdow_mem_input_ctler.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_wdow_mem_output_ctler.vhd \
+ 
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_clk_mem_ctrler.vhd \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_clks_memory.bde \
+ d:\Telops\Common_HDL\Common_Projects\TEL2000\FPA_common\src\fastrd2\fastrd2_wdow_info_rate_ctrler.bde \
+ d:\Telops\Common_HDL\Common_Proje+
+ 
+ +
--- Common_Projects/TEL2000/FPA_common/src/fastrd2/fastrd2_misc_flags_gen.vhd
+++ Common_Projects/TEL2000/FPA_common/src/fastrd2/fastrd2_misc_flags_gen.vhd
@@ -17,25 +17,23 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 use work.fastrd2_define.all; 
 
-entity fastrd2_clk_area_gen is
+entity fastrd2_misc_flags_gen is
    generic(
-      CLK_AREA_ID       : integer range 0 to FPA_MCLK_NUM_MAX-1 := 0
+      AREA_CLK_ID       : integer range 0 to FPA_MCLK_NUM_MAX-1 := 0
       );   
    port (
       ARESET            : in std_logic;
       CLK               : in std_logic; 
       
-      CLK_AREA_CFG      : in area_cfg_type;
-      
       AREA_INFO_I       : in area_info_type;      
       AREA_INFO_O       : out area_info_type    
       );  
-end fastrd2_clk_area_gen;
+end fastrd2_misc_flags_gen;
 
 
-architecture rtl of fastrd2_clk_area_gen is   
+architecture rtl of fastrd2_misc_flags_gen is   
    
-   type area_info_pipe_type is array (0 to 3) of area_info_type;
+   type area_info_pipe_type is array (0 to 2) of area_info_type;
    
    component sync_reset
       port(
@@ -46,14 +44,13 @@ architecture rtl of fastrd2_clk_area_gen is
    
    signal sreset               : std_logic;
    signal area_info_pipe       : area_info_pipe_type;
-   signal clk_stamp_en         : std_logic_vector(3 downto 0);
    
 begin
    
    --------------------------------------------------
    -- Outputs map
    --------------------------------------------------                       
-   AREA_INFO_O <= area_info_pipe(3);   -- pour fins de synchro
+   AREA_INFO_O <= area_info_pipe(1);   
    
    --------------------------------------------------
    -- synchro reset 
@@ -66,7 +63,7 @@ begin
       ); 
    
    --------------------------------------------------
-   --  generation des identificateurs de trames 
+   --  generation des identificateurs 
    --------------------------------------------------
    U5: process(CLK)
    begin
@@ -76,51 +73,27 @@ begin
             for ii in 0 to 3 loop
                area_info_pipe(ii).raw <= ((others => '0'), '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', (others => '0'), (others => '0'), '0');
                area_info_pipe(ii).user <= ((others => '0'), '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', (others => '0'), (others => '0'), '0');
-               clk_stamp_en <= (others => '0');
             end loop;
             -- pragma translate_on
             
          else           
             
             --------------------------------------------------------
-            -- pipe 0 : reperage premiere ligne à marquer
+            -- pipe 0 : 
             ------------------------------------------------
-            area_info_pipe(0) <= AREA_INFO_I;
-            if  AREA_INFO_I.RAW.LINE_CNT >= CLK_AREA_CFG.LINE_START_NUM then 
-               clk_stamp_en(0) <= '1';            
-            else                       
-               clk_stamp_en(0) <= '0';
-            end if;                  
+            area_info_pipe(0) <= AREA_INFO_I;                
             
             --------------------------------------------------------
-            -- pipe 1 : reperage derniere ligne à marquer (fin)
+            -- pipe 1 : imminent_clk_change and imminent_aoi
             --------------------------------------------------------      
             area_info_pipe(1) <= area_info_pipe(0);        
-            if  area_info_pipe(0).raw.line_cnt <= CLK_AREA_CFG.LINE_END_NUM then 
-               clk_stamp_en(1) <= clk_stamp_en(0);
+            if AREA_INFO_I.CLK_ID /= area_info_pipe(0).clk_id then 
+               area_info_pipe(1).raw.imminent_clk_change <= '1';
             else
-               clk_stamp_en(1) <= '0';
-               clk_stamp_en(0) <= '0';
+               area_info_pipe(1).raw.imminent_clk_change <= '0';
             end if;         
-            
-            --------------------------------------------------------
-            -- pipe 2 : reperage des colonnes à marquer
-            --------------------------------------------------------
-            area_info_pipe(2) <= area_info_pipe(1);
-            if area_info_pipe(1).raw.line_pclk_cnt = CLK_AREA_CFG.SOL_POSL_PCLK then
-               clk_stamp_en(2) <= clk_stamp_en(1);
-            elsif area_info_pipe(1).raw.line_pclk_cnt = CLK_AREA_CFG.EOL_POSL_PCLK_P1 then
-               clk_stamp_en(2) <= '0';
-            end if;  
-            
-            --------------------------------------------------------
-            -- pipe 3 : marquage des zones
-            --------------------------------------------------------
-            area_info_pipe(3) <= area_info_pipe(2);
-            if clk_stamp_en(2) = '1' then
-               area_info_pipe(3).clk_id <= to_unsigned(CLK_AREA_ID, area_info_pipe(3).clk_id'length);
-            end if; 
-            
+            area_info_pipe(1).raw.imminent_aoi <= not area_info_pipe(0).user.sol and AREA_INFO_I.USER.SOL;
+                        
             
          end if;
       end if;
